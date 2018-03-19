72bea655e15b6887189c901bda8c063b50af8690
Galamon5 <galamon5@hotmail.com>
2018-03-19 16:24:46 -0600

    Creacion de nuevo proyecto. Error npm start solucionado

---
+++ App_Escom/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Navigator, NativeModules } from 'react-native';
+
+import { COLOR, ThemeProvider } from '../react-native-material-ui';
+
+// you can set your style right here, it'll be propagated to application
+const uiTheme = {
+    palette: {
+        primaryColor: COLOR.green500,
+    },
+    toolbar: {
+        container: {
+            height: 50,
+        },
+    },
+};
+
+class Main extends Component {
+    render() {
+        return (
+            <ThemeProvider uiTheme={uiTheme}>
+                <App />
+            </ThemeProvider>
+        );
+    }
+}
