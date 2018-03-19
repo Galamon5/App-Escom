8656d545f1ee0da1f083d26e7723578ac3486135
Galamon5 <galamon5@hotmail.com>
2018-03-19 02:00:43 -0600

    Prueba de git en atom

--- AppEscom/App.js
+++ AppEscom/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Que onda que pez amijo!</Text>
+        <Text>Empezando proyectos!</Text>
       </View>
     );
   }
